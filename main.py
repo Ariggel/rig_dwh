005b9af03df4d1fcbf67de8b85e05a76f0e29675
Sascha <saschaavsn@gmail.com>
2026-06-27 13:07:18 +0200

    feat: :sparkles: upload function for raw data
    Implemented a loader job for raw data from data sources into the database. Loads a pandas DataFrame into a SQL Server database table.. -- Separated the creation of the sql engine.  Creates a SQLAlchemy engine for connecting to a SQL Server database.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from src.orchestration import destatis_job
 import os
 from dotenv import load_dotenv
 
+from src.loaders.sql_loader import create_sql_engine, load_dataframe
+from src.orchestration import destatis_job
 
 load_dotenv()
+engine = create_sql_engine()
+
 
 results = destatis_job.run()
+
+
+
+
+for table, payload in results.items():
+    load_dataframe(
+         data_frame = payload["data_frame"]
+        ,schema_name="RAW"
+        ,table_name = payload["table_name_db"]
+        ,engine = engine
+    )
+    
---
+++ src/loaders/sql_loader/__init__.py
@@ -0,0 +1,2 @@
+from .loader_job import load_dataframe
+from .create_sql_engine import create_sql_engine
