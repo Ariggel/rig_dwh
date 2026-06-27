ab60866caec2ebcdc3ccc8e6c75b98a19582c8ff
Sascha <saschaavsn@gmail.com>
2026-06-27 02:53:41 +0200

    feat: :sparkles: table name for database
    implented a variable for the table name inside the dwh database in the settings of the selected raw tables

--- config/destatis_data.py
+++ config/destatis_data.py
@@ -1,26 +1,30 @@
 TABLE_CONFIG = {
-    "61111-0001": {
-        "year_start": 2010
-        ,"year_end": 2025
-        ,"language_data": "de"
-        ,"table_name": "Verbraucherpreisindex: Deutschland, Jahre"
-    },
+    #"61111-0001": {
+    #    "year_start": 2010
+    #    ,"year_end": 2025
+    #    ,"language_data": "de"
+    #    ,"table_name": "Verbraucherpreisindex: Deutschland, Jahre"
+    #    ,"table_name_db": "DATA_DESTATIS_CONSUMER_PRICE_INDEX"
+    #},
     "12211-0902": {
          "year_start": 2017
          ,"year_end": 2025
          ,"language_data": "de"
          ,"table_name": "Bevölkerung in Gemeinschaftsunterkünften: Bundesländer, Jahre, Hauptstatus, Geschlecht, Altersgruppen"
+         ,"table_name_db": "DATA_DESTATIS_COLLECTIVE_HOUSING_STATUS"
     },
     "12211-0903": {
         "year_start": 2017 
         ,"year_end": 2025
         ,"language_data": "de"
         ,"table_name": "Bevölkerung in Gemeinschaftsunterkünften: Bundesländer, Jahre, Art der Gemeinschaftsunterkunft, Geschlecht"
+        ,"table_name_db": "DATA_DESTATIS_COLLECTIVE_HOUSING_KIND"
     },
     "12211-1001": {
         "year_start": 2021
         ,"year_end": 2025
         ,"language_data": "de"
         ,"table_name": "Bevölkerung, Erwerbstätige, Erwerbslose, Erwerbspersonen, Nichterwerbspersonen aus Hauptwohnsitzhaushalten: Bundesländer, Jahre, Geschlecht, Altersgruppen"
+        ,"table_name_db": "DATA_DESTATIS_LABOUR_FORCE"
     }  
 }
